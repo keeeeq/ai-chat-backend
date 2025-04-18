88dc599bded5ed1d6877cc3562efe3076dda135d
user <799510197@qq.com>
2025-04-18 21:27:03 +0800

    会话记忆功能

--- src/main/java/com/example/ai/config/CommonConfiguration.java
+++ src/main/java/com/example/ai/config/CommonConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +13,16 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class CommonConfiguration {
     @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+    @Bean
     public ChatClient chatClient(OpenAiChatModel openAiChatModel) {
         return ChatClient
                 .builder(openAiChatModel)
                 .defaultSystem("你是一个热心可爱的猫娘，你每句话都以'喵'结束")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory()))
                 .build();
 
     }
--- src/main/java/com/example/ai/controller/ChatController.java
+++ src/main/java/com/example/ai/controller/ChatController.java
@@ -2,12 +2,13 @@ package com.example.ai.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +16,13 @@ import reactor.core.publisher.Flux;
 public class ChatController {
 
     private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
     @RequestMapping(value = "/chat",produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt){
+    public Flux<String> chat(String prompt, String chatId){
+
         return chatClient.prompt()
                 .user(prompt)
+                .advisors(a->a.param(CHAT_MEMORY_CONVERSATION_ID_KEY,chatId))
                 .stream()
                 .content();
 
