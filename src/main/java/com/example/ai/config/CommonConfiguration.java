e54d825f3eb879649c1cc84632e845d1af030e3e
user <799510197@qq.com>
2025-05-30 19:22:46 +0800

    rag实现个人知识库功能

--- src/main/java/com/example/ai/config/CommonConfiguration.java
+++ src/main/java/com/example/ai/config/CommonConfiguration.java
@@ -5,13 +5,20 @@ import com.example.ai.constants.SystemConstants;
 import com.example.ai.tools.NovelTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Vector;
 
 
 @Configuration
@@ -60,5 +67,35 @@ public class CommonConfiguration {
                 .defaultTools(novelTools)
                 .build();
     }
+/**
+ *
+ 配置向量数据库
+ *
+ */
+    @Bean
+    public VectorStore vectorStore(OpenAiEmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
 
+    @Bean
+    public ChatClient pdfChatClient(OpenAiChatModel openAiChatModel,VectorStore vectorStore) {
+        return ChatClient
+                .builder(openAiChatModel)
+                .defaultSystem("根据上下文回答问题，遇到上下文没有的问题，不要随意编造")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        //日志
+                        new MessageChatMemoryAdvisor(chatMemory()),
+                        new QuestionAnswerAdvisor(
+                                vectorStore,
+                                SearchRequest.builder()
+                                        .similarityThreshold(0.6)
+                                        .topK(5)
+                                        .build()
+                                )
+
+                )
+                //会话记忆
+                .build();
+    }
 }
